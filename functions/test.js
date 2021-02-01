37f0f3607ac2b019d507cf944d0b38c72f353f04
Brian Boros <Bpower@tests-MBP.attlocal.net>
2021-01-31 21:14:07 -0600

    added firebase config to test.js

--- functions/test.js
+++ functions/test.js
@@ -4,6 +4,20 @@ const serviceAccount = require("path/to/serviceAccountKey.json")
 // Initialise the admin with the credentials
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL:
-  "
+  databaseURL: "firebase-adminsdk-nfyo4@brian-boros-nextjs.iam.gserviceaccount.com"
 })
+
+const db = admin.firestore()
+
+exports.handler = async (event, context, callback) => {
+  await db.collection('COLLECTION').add({
+    name: 'Test'
+  })
+
+  return callback(null, {
+    statusCode: 200,
+    body: JSON.stringify({
+      data: `Test data added successfully`
+    })
+  })
+}
