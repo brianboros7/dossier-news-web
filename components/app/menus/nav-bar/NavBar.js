16d93e63f0055dacc336c061cfa43384e8618d03
Brian Boros <Bpower@tests-MBP.attlocal.net>
2021-02-02 10:12:03 -0600

    added to NavBar

--- components/app/menus/nav-bar/NavBar.js
+++ components/app/menus/nav-bar/NavBar.js
@@ -4,23 +4,23 @@ export default function NavBar() {
 
     return(
         <Navbar bg="light" expand="lg">
-            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Brand href="#home">Liberty News</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse>
                 <Nav className="mr-auto">
                     <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#link">Link</Nav.Link>
-                    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                    <Nav.Link href="#link">US</Nav.Link>
+                    <Nav.Link href="#link">World</Nav.Link>
+
+                    <NavDropdown title="Politics" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="#action/3.3">International</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.1">Policy and Government</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.2">War and Conflict</NavDropdown.Item>
                         <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.4">C-Span</NavDropdown.Item>
                     </NavDropdown>
                 </Nav>
             </Navbar.Collapse>
-        </Navbar>
-            
-            
+        </Navbar>        
     )
 }
