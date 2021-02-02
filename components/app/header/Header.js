51a8390782d58735a915150ed8819ffbd06df25c
Brian Boros <Bpower@tests-MBP.attlocal.net>
2021-02-02 00:06:50 -0600

    added bootstrap works, header and navbar components

---
+++ components/app/header/Header.js
@@ -0,0 +1,10 @@
+import NavBar from '../menus/nav-bar/NavBar'
+
+export default function Header() {
+
+    return(
+        <header className="fluid">
+            <NavBar /> 
+        </header>
+    )
+}
