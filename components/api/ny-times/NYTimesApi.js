6d0692bbc12b012d90cf89b347bd1ad61140d40c
Brian Boros <Bpower@tests-MBP.attlocal.net>
2021-02-11 15:13:06 -0600

    NY times api is working

--- components/api/ny-times/NYTimesApi.js
+++ components/api/ny-times/NYTimesApi.js
@@ -1,29 +1,24 @@
-import {useEffect, useState} from 'react'
+import {useState, useEffect} from 'react'
 
 function NYTimesApi() {
     const [results, setNews] = useState([])
 
     const url = "https://api.nytimes.com/svc/news/v3/content/nyt/world.json?api-key=irsGJwPj6mA7c6Hkbs3o2jejFAJBjNzf"
-    const loadDate = () => {
+    const loadData = () => {
         fetch(url)
-        .then((response) => response.json)
-        .then((data) => setNews(data.results))
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data)
+            setNews(data.results)
+        })
     }
 
     useEffect(() => {
         loadData()
-    })
+    }, []) 
 
-    const newsArticles = results.map((result, index) => (
-        <Card key={index} className={styles["news-card"]} style={{ width: '20rem'}}>
-            <Card.Body>
-                <Card.Img src={result.thumbnail_standard}></Card.Img>
-                <Card.Text>{result.abstract}</Card.Text>
-                <Card.Title>{result.title}</Card.Title>
-
-                <Card.Text>{result.published_date}</Card.Text>
-            </Card.Body>
-        </Card>
+    let newsArticles = results.map((result, index) => (
+        <div>{result.abstract}</div>
     ))
 
     return(
